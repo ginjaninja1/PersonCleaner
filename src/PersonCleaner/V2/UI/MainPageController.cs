75b6c5b75d971cbe22c4668daf1a8794cf2bb61b
ginjaninja1 <nick@birdsglobal.com>
2026-08-28 14:51:04 +0100

    menud

--- src/PersonCleaner/V2/UI/MainPageController.cs
+++ src/PersonCleaner/V2/UI/MainPageController.cs
@@ -20,10 +20,10 @@ namespace PersonCleaner.V2.UI
         public MainPageController(PluginInfo plugin, IServerApplicationHost host, ILogger logger) : base(plugin.Id)
         {
             this.plugin = plugin; this.host = host; this.logger = logger;
-            PageInfo = new PluginPageInfo { Name = "personcleaner", DisplayName = "PersonCleaner", EnableInMainMenu = false, MenuIcon = "people", IsMainConfigPage = true };
+            PageInfo = new PluginPageInfo { Name = "personcleaner", DisplayName = "PersonCleaner", EnableInMainMenu = true, MenuIcon = "people", IsMainConfigPage = true };
             tabs = new List<IPluginUIPageController>
             {
-                new TabController(plugin, "personcleaner-config-v2", "Configuration", x => new ConfigurationPageView(x, host, logger)),
+                //new TabController(plugin, "personcleaner-config-v2", "Configuration", x => new ConfigurationPageView(x, host, logger)),
                 new TabController(plugin, "personcleaner-evidence-v2", "Decision evidence", x => new EvidencePageView(x, host, logger)),
                 new TabController(plugin, "personcleaner-corrections-v2", "Provider corrections", x => new CorrectionsPageView(x, host, logger))
             };
